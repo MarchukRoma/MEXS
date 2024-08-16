7435e46b64a2b2253c866d3f3feebb8d5f66f304
unknown <marchukroman0036@gmail.com>
2024-08-16 13:37:55 +0300

    maybe work

--- src/components/OwnerInfo.jsx
+++ src/components/OwnerInfo.jsx
@@ -1,17 +1,16 @@
 function OwnerInfo() {
   const ownerName = "Тарас Тарасович";
   return (
-    <div style={{ marginBottom: "10px" }}>
+    <>
       <img id="ownerImg" src="owner.png" alt="ownerImg" />
-      <p id="ownerName">
-        <h1>
-          {ownerName}
-          <img id="tick" src="tick.png" alt="tick" />
-        </h1>
-      </p>
+
+      <h1 id="ownerName">
+        {ownerName}
+        <img id="tick" src="tick.png" alt="tick" />
+      </h1>
 
       {/* <img src="./img/golden_tick.png" alt="Logo" /> */}
-    </div>
+    </>
   );
 }
 export default OwnerInfo;
